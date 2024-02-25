21f52f6d0764b34e716fe4fb9fa87850d3f35e7d
royalistofficial <mishabygaga1414@gmail.com>
2024-02-25 15:11:24 +0300

    connected the database

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,13 +77,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'warehouse',
         'USER': 'royalist',
@@ -92,7 +85,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    
     return render(request, 'main/index.html')
 
 def about(request):
